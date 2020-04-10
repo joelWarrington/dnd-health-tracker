67d61ad311437c7eb10db4272c90578475fc238e
joelWarrington <46493033+joelWarrington@users.noreply.github.com>
2020-04-10 05:05:41 -0600

    updated eslint rule

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,11 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
