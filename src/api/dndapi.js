8ccc5d12d2709c3d5ccc299fbeb776ff79c340dd
joelWarrington <46493033+joelWarrington@users.noreply.github.com>
2020-04-12 03:15:17 -0600

    Added common/basic API ressources to be used through application
    Have added methods to search most of the ressources on http://www.dnd5eapi.co/ that i'll need for this application
    
    Resolves #1

--- src/api/dndapi.js
+++ src/api/dndapi.js
@@ -2,17 +2,42 @@
 /* global dndapi:writable */
 /* exported dndapi */
 module.exports = {
-  baseUrl: 'http://www.dnd5eapi.co/api/',
-  async getMonsters() {
-    const endpoint = 'monsters';
-    const response = await fetch(this.baseUrl + endpoint);
+  baseUrl: 'http://www.dnd5eapi.co/api',
+  async apiCall(url) {
+    const response = await fetch(url);
     const responseJSON = response.json();
-    return responseJSON;
+    return responseJSON.results;
   },
-  async getMonster(id) {
-    const endpoint = `monsters/ + ${id.toString}`;
-    const response = await fetch(this.baseUrl + endpoint);
-    const responseJSON = response.json();
-    return responseJSON;
+  async getMonsters() {
+    const monsters = await this.apiCall(`${this.baseUrl}/monsters`);
+    return monsters;
+  },
+  async getMonster(index) {
+    const monster = await this.apiCall(`${this.baseUrl}/monsters/${index.toString()}`);
+    return monster;
+  },
+  async queryMonstersName(name) {
+    const monsters = await this.apiCall(`${this.baseUrl}/monsters?${encodeURIComponent(name)}`);
+    return monsters;
+  },
+  async getAbilityScores() {
+    const abilities = await this.apiCall(`${this.baseUrl}/ability-scores`);
+    return abilities;
+  },
+  async getConditions() {
+    const conditions = await this.apiCall(`${this.baseUrl}/conditions`);
+    return conditions;
+  },
+  async getCondition(index) {
+    const condition = await this.apiCall(`${this.baseUrl}/conditions/${index}}`);
+    return condition;
+  },
+  async getDamageTypes() {
+    const damageTypes = await this.apiCall(`${this.baseUrl}/damage-types`);
+    return damageTypes;
+  },
+  async getDamageType(index) {
+    const damageType = await this.apiCall(`${this.baseUrl}/damage-types/${index}`);
+    return damageType;
   },
 };
