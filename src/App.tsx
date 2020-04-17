a12058d1e1453bb6cc53ccc978c6ebc3c85b2419
joelWarrington <46493033+joelWarrington@users.noreply.github.com>
2020-04-17 01:11:30 -0600

    added grid for page and menu which links back to repo

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,40 @@ import React from 'react';
 import AddMonster from './components/AddMonster';
 import './App.scss';
 import { AppContextProvider } from './AppContext';
+import { Menu, Container, Image, Grid, Segment } from 'semantic-ui-react';
 
 function App() {
   return (
-    <AppContextProvider>
-      <AddMonster />
-    </AppContextProvider>
+    <>
+      <AppContextProvider>
+        <Menu borderless size="small" fluid inverted>
+          <Container>
+            <Menu.Item content="D&D Health Tracker" header />
+            <Menu.Item
+              as="a"
+              position="right"
+              href="https://github.com/joelWarrington/dnd-health-tracker"
+              target="_blank"
+            >
+              <Image
+                size="mini"
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                circular
+              ></Image>
+            </Menu.Item>
+          </Container>
+        </Menu>
+        <Container>
+          <Grid columns={16} padded="horizontally" divided>
+            <Grid.Column width={10}>
+              <Grid.Row>
+                <AddMonster />
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </AppContextProvider>
+    </>
   );
 }
 
