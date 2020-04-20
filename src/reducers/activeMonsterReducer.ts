25a3b8a181ab7529f2b4b46478781b48655506fe
joelWarrington <46493033+joelWarrington@users.noreply.github.com>
2020-04-20 05:01:17 -0600

    added action to update active monster

--- src/reducers/activeMonsterReducer.ts
+++ src/reducers/activeMonsterReducer.ts
@@ -1,7 +1,14 @@
+import _ from 'lodash';
+
 const activeMonsterReducer = (state: any, action: any) => {
   switch (action.type) {
     case 'ADD_MONSTER':
       return [...state, action.monster];
+    case 'UPDATE_MONSTER':
+      return _.map(state, (monster, index) => {
+        if (monster.id === action.id) return action.monster;
+        else return monster;
+      });
     default:
       return state;
   }
